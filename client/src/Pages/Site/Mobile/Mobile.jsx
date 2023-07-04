e2dc89b2864b075cf787a854e66dd97537bb6818
Orxan <orxan.qasimov.2003@gmail.com>
2023-07-04 11:07:43 +0400

    commit

--- client/src/Pages/Site/Mobile/Mobile.jsx
+++ client/src/Pages/Site/Mobile/Mobile.jsx
@@ -4,18 +4,7 @@ import { BiSearchAlt } from "react-icons/bi";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import "./Mobile.scss";
 import MetaData from "../../../Layout/Site/Header/MetaData";
-import MobileCard from "./MobileCard";
-
-const product ={
-    name:"NEW RAZER BLADE 14",
-    descCard: "NVIDIA® GeForce RTX™ 40 Series 14” Laptop with AMD Ryzen 9 7940HS Processor.",
-    imgCard: "https://assets3.razerzone.com/HWyBrdi1Zay37XldxX3Y1O4puOg=/500x500/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fhb7%2Fh1b%2F9539342008350%2Fblade14-p9-black-500x500.png",
-    price:2399.99,
-    category:"pc",
-    _id:"123",
-  }
-
-
+import axios from "axios";
 const Mobile = () => {
   const [loading, setLoading] = useState(false);
   useEffect(() => {
@@ -26,6 +15,28 @@ const Mobile = () => {
     }, 1500);
   }, []);
 
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:7777/api/v1/product/").then((res) => {
+      console.log(res.data);
+      setData(res.data);
+    }).catch((error) => {
+            console.error("Error fetching data:", error);
+  });
+ }, []);
+
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:7777/api/v1/product/")
+  //     .then((res) => {
+  //       console.log(res.data); // Check the response in the console
+  //       setData(res.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching data:", error);
+  //     });
+  // }, []);
   return (
     <>
       {loading ? (
@@ -48,143 +59,37 @@ const Mobile = () => {
               <p>The Ultimate Android Gaming Handheld</p>
             </div>
             <div className="mobile__vitrine">
-              <MobileCard product={product}/>
-              <MobileCard product={product}/>
-              <MobileCard product={product}/>
-              <MobileCard product={product}/>
-              <MobileCard product={product}/>
-              {/* <div className="mobile__vitrine__card">
-                <div className="mobile__vitrine__card--top">
-                  <img
-                    src="https://assets3.razerzone.com/HWyBrdi1Zay37XldxX3Y1O4puOg=/500x500/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fhb7%2Fh1b%2F9539342008350%2Fblade14-p9-black-500x500.png"
-                    alt=""
-                  />
-                </div>
-                <div className="mobile__vitrine__card--bottom">
-                  <div className="mobile__vitrine__card-name">
-                    <p>Razer Blade 18 Models</p>
-                  </div>
-                  <div className="mobile__vitrine__card-desc">
-                    <p>
-                      NVIDIA® GeForce RTX™ 40 Series 14” Laptop with AMD Ryzen 9
-                      7940HS Processor
-                    </p>
-                  </div>
-                  <div className="mobile__vitrine__card-priceAdd">
-                    <div className="mobile__vitrine__card-priceAdd-left">
-                      <p>From</p>
-                      <p>US$3,799</p>
+              { data.map((item, index) => {
+                return (
+                  <div key={index} className="mobile__vitrine__card">
+                    <div className="mobile__vitrine__card--top">
+                      <img src={item.imgCard} alt="" />
                     </div>
-                    <div className="mobile__vitrine__card-priceAdd-right">
-                      <button>
-                        <BiSearchAlt className="addToCardIco" />
-                      </button>
-                      <button>
-                        <AiOutlineShoppingCart className="addToCardIco" />
-                      </button>
+                    <div className="mobile__vitrine__card--bottom">
+                      <div className="mobile__vitrine__card-name">
+                        <p>{item.name}</p>
+                      </div>
+                      <div className="mobile__vitrine__card-desc">
+                        <p>{item.descCard}</p>
+                      </div>
+                      <div className="mobile__vitrine__card-priceAdd">
+                        <div className="mobile__vitrine__card-priceAdd-left">
+                          <p>From</p>
+                          <p>US${item.imgCard}</p>
+                        </div>
+                        <div className="mobile__vitrine__card-priceAdd-right">
+                          <button>
+                            <BiSearchAlt className="addToCardIco" />
+                          </button>
+                          <button>
+                            <AiOutlineShoppingCart className="addToCardIco" />
+                          </button>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-              <div className="mobile__vitrine__card">
-                <div className="mobile__vitrine__card--top">
-                  <img
-                    src="https://assets3.razerzone.com/HWyBrdi1Zay37XldxX3Y1O4puOg=/500x500/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fhb7%2Fh1b%2F9539342008350%2Fblade14-p9-black-500x500.png"
-                    alt=""
-                  />
-                </div>
-                <div className="mobile__vitrine__card--bottom">
-                  <div className="mobile__vitrine__card-name">
-                    <p>Razer Blade 18 Models</p>
-                  </div>
-                  <div className="mobile__vitrine__card-desc">
-                    <p>
-                      NVIDIA® GeForce RTX™ 40 Series 14” Laptop with AMD Ryzen 9
-                      7940HS Processor
-                    </p>
-                  </div>
-                  <div className="mobile__vitrine__card-priceAdd">
-                    <div className="mobile__vitrine__card-priceAdd-left">
-                      <p>From</p>
-                      <p>US$3,799</p>
-                    </div>
-                    <div className="mobile__vitrine__card-priceAdd-right">
-                      <button>
-                        <BiSearchAlt className="addToCardIco" />
-                      </button>
-                      <button>
-                        <AiOutlineShoppingCart className="addToCardIco" />
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="mobile__vitrine__card">
-                <div className="mobile__vitrine__card--top">
-                  <img
-                    src="https://assets3.razerzone.com/HWyBrdi1Zay37XldxX3Y1O4puOg=/500x500/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fhb7%2Fh1b%2F9539342008350%2Fblade14-p9-black-500x500.png"
-                    alt=""
-                  />
-                </div>
-                <div className="mobile__vitrine__card--bottom">
-                  <div className="mobile__vitrine__card-name">
-                    <p>Razer Blade 18 Models</p>
-                  </div>
-                  <div className="mobile__vitrine__card-desc">
-                    <p>
-                      NVIDIA® GeForce RTX™ 40 Series 14” Laptop with AMD Ryzen 9
-                      7940HS Processor
-                    </p>
-                  </div>
-                  <div className="mobile__vitrine__card-priceAdd">
-                    <div className="mobile__vitrine__card-priceAdd-left">
-                      <p>From</p>
-                      <p>US$3,799</p>
-                    </div>
-                    <div className="mobile__vitrine__card-priceAdd-right">
-                      <button>
-                        <BiSearchAlt className="addToCardIco" />
-                      </button>
-                      <button>
-                        <AiOutlineShoppingCart className="addToCardIco" />
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="mobile__vitrine__card">
-                <div className="mobile__vitrine__card--top">
-                  <img
-                    src="https://assets3.razerzone.com/HWyBrdi1Zay37XldxX3Y1O4puOg=/500x500/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fhb7%2Fh1b%2F9539342008350%2Fblade14-p9-black-500x500.png"
-                    alt=""
-                  />
-                </div>
-                <div className="mobile__vitrine__card--bottom">
-                  <div className="mobile__vitrine__card-name">
-                    <p>Razer Blade 18 Models</p>
-                  </div>
-                  <div className="mobile__vitrine__card-desc">
-                    <p>
-                      NVIDIA® GeForce RTX™ 40 Series 14” Laptop with AMD Ryzen 9
-                      7940HS Processor
-                    </p>
-                  </div>
-                  <div className="mobile__vitrine__card-priceAdd">
-                    <div className="mobile__vitrine__card-priceAdd-left">
-                      <p>From</p>
-                      <p>US$3,799</p>
-                    </div>
-                    <div className="mobile__vitrine__card-priceAdd-right">
-                      <button>
-                        <BiSearchAlt className="addToCardIco" />
-                      </button>
-                      <button>
-                        <AiOutlineShoppingCart className="addToCardIco" />
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div> */}
+                );
+              })}
             </div>
             <div className="mobile__sync">
               <p>STAY IN-SYNC WITH YOUR WORLD</p>
--- server/app.js
+++ server/app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+  });
+
 const errorMiddleware = require("./middleware/error")
 
 //Route Imports
